ad00d9eaa7f7b43d28def612675fd3e755b13268
Manik Roy <pixnum@gmail.com>
2026-02-03 16:06:41 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,33 @@
-const CACHE_NAME = "barcodic-cache-v1";
-const urlsToCache = [
+const CACHE_NAME = "barcodic-v1";
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js"
 ];
 
+// Install SW and cache assets
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+// Activate SW and remove old caches
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+          if(key !== CACHE_NAME) return caches.delete(key);
         })
       )
     )
   );
 });
 
+// Fetch from cache first, fallback to network
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
